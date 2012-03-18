49e426dabe3128294c63ee82d987cc6adaf0f358
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-03-18 20:50:56 +0100

    Fixed method imageWithRoundCornersOfImage.

--- UIImageHelper.m
+++ UIImageHelper.m
@@ -216,47 +216,19 @@ void drawGlossAndGradient(CGContextRef context, CGRect rect, CGColorRef startCol
     return result;
 }
 
-void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth, float ovalHeight);
-void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth, float ovalHeight) {
-	float fw, fh;
-	if (ovalWidth == 0 || ovalHeight == 0) {
-		CGContextAddRect(context, rect);
-		return;
-	}
-	CGContextSaveGState(context);
-	CGContextTranslateCTM (context, CGRectGetMinX(rect), CGRectGetMinY(rect));
-	CGContextScaleCTM (context, ovalWidth, ovalHeight);
-	fw = CGRectGetWidth (rect) / ovalWidth;
-	fh = CGRectGetHeight (rect) / ovalHeight;
-	CGContextMoveToPoint(context, fw, fh/2);
-	CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);
-	CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1);
-	CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1);
-	CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1);
-	CGContextClosePath(context);
-	CGContextRestoreGState(context);
-}
-
 + (UIImage *)imageWithRoundCornersOfImage:(UIImage *)source {
-	int w = source.size.width;
-	int h = source.size.height;
-	
-	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-	CGContextRef context = CGBitmapContextCreate(NULL, w, h, 8, 4 * w, colorSpace, kCGImageAlphaPremultipliedFirst);
-	
-	CGContextBeginPath(context);
-	CGRect rect = CGRectMake(0, 0, w, h);
-	addRoundedRectToPath(context, rect, 5, 5);
-	CGContextClosePath(context);
-	CGContextClip(context);
-	
-	CGContextDrawImage(context, CGRectMake(0, 0, w, h), source.CGImage);
-	
-	CGImageRef imageMasked = CGBitmapContextCreateImage(context);
-	CGContextRelease(context);
-	CGColorSpaceRelease(colorSpace);
-	
-	return [UIImage imageWithCGImage:imageMasked];    
+    CGRect rect = CGRectMake(0, 0, CGImageGetWidth([source CGImage]), CGImageGetHeight([source CGImage]));
+	UIGraphicsBeginImageContext(rect.size);
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:rect byRoundingCorners:UIRectCornerAllCorners cornerRadii:CGSizeMake(25, 25)];
+    [path addClip];
+    [source drawAtPoint:CGPointMake(0, 0)];
+    
+    UIImage *roundedImage = UIGraphicsGetImageFromCurrentImageContext();
+
+    UIGraphicsEndImageContext();
+
+    return roundedImage;
 }
 
 + (UIImage *)imagePixelWithColor:(UIColor *)color {
